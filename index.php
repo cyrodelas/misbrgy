1065d63c10ec51167118f483be9ecbcac38abb56
cyrodelas <cyrodelasluna@gmail.com>
2021-04-30 12:01:51 +0800

    second upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <style>
+            body{
+                background-color: black;
+            }
+            p{
+                color: white;
+            }
+        </style>
+    </head>
+    <body>
+        <p><?php echo "Hello World"; ?></p>
+        
+    </body>
+</html>
